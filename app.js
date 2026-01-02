bf9e29c4a729db40a9a5e26044b7d7de7d536115
David K <davidupdesign@gmail.com>
2026-01-03 00:01:06 +0400

    main logic done

--- app.js
+++ app.js
@@ -5,28 +5,49 @@ const startPauseButton = document.querySelector("#start-pause-button");
 const stopButton = document.querySelector("#stop-button");
 const resetButton = document.querySelector("#reset-button");
 
+const sound = new Audio(
+  "/src/magical-notification-tone-soft-fantasy-digital-alert-438278.mp3"
+);
+const tenSecSound = new Audio("/src/notification-alert-6-331724.mp3");
+
+const workSessionInput = document.querySelector("#work-session");
+const breakSessionInput = document.querySelector("#break-session");
+
 let isWorkSession = true;
-let timeLeft = 1500;
+let timeLeft = workSessionInput.value * 60;
 let isTimerRunning = false;
 let intervalId;
+
+let workSessionsCompleted = 0;
 
 function startTimer() {
   intervalId = setInterval(() => {
     if (timeLeft === 0) {
       clearInterval(intervalId);
+      sound.play();
       if (isWorkSession) {
+        workSessionsCompleted += 1;
         isWorkSession = false;
-        timeLeft = 300;
-        sessionType.textContent = "Break Session";
+        if (workSessionsCompleted % 4 === 0) {
+          timeLeft = 600;
+          sessionType.textContent = "Long Break";
+        } else {
+          timeLeft = breakSessionInput.value * 60;
+          sessionType.textContent = "Break Session";
+        }
       } else {
         isWorkSession = true;
-        timeLeft = 1500;
+        timeLeft = workSessionInput.value * 60;
         sessionType.textContent = "Work Session";
       }
+
       updateDisplay();
       startTimer();
     } else {
       timeLeft -= 1;
+      if (timeLeft === 10) {
+        tenSecSound.play();
+      }
       updateDisplay();
     }
   }, 1000);
@@ -56,11 +77,11 @@ stopButton.addEventListener("click", () => {
   if (isWorkSession) {
     clearInterval(intervalId);
     isTimerRunning = false;
-    timeLeft = 1500;
+    timeLeft = workSessionInput.value * 60;
   } else {
     clearInterval(intervalId);
     isTimerRunning = false;
-    timeLeft = 300;
+    timeLeft = breakSessionInput.value * 60;
   }
   updateDisplay();
 });
@@ -71,5 +92,23 @@ resetButton.addEventListener("click", () => {
   isTimerRunning = false;
   timeLeft = 1500;
   sessionType.textContent = "Work Session";
+  workSessionInput.value = 25;
+  breakSessionInput.value = 5;
   updateDisplay();
+});
+
+// for inpiuts
+
+workSessionInput.addEventListener("input", () => {
+  if (isWorkSession && !isTimerRunning) {
+    timeLeft = workSessionInput.value * 60;
+    updateDisplay();
+  }
+});
+
+breakSessionInput.addEventListener("input", () => {
+  if (!isWorkSession && !isTimerRunning) {
+    timeLeft = breakSessionInput.value * 60;
+    updateDisplay();
+  }
 });
